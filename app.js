807d32e436e2cb63af21a87e4c558666eb7beb40
Jerome DESMARES <jdesmare@student.42.fr>
2017-06-25 00:35:16 +0200

    chart + image

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const https = require('https');
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
 const app = express();
+const api = "http://fd6ba847.ngrok.io/api/get?url_input_page=";
+const Client = require('node-rest-client').Client;
+const client = new Client();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -13,7 +16,11 @@ app.get('/', function (req, res) {
 })
 
 app.get('/result', function (req, res) {
-	res.render('result');
+	client.get("http://fd6ba847.ngrok.io/api/get?url_input_page=" + req.query.id, function (data, response) {
+		let object = JSON.parse(data);
+		console.log(object);
+		res.render('result', {json: object});
+});
 })
 
 app.listen(3000, function () {
